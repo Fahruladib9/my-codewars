b3817720a6b1b7e82fe5d1c04dc3c7a41b9ce27c
fahrul adib <fahruladib9@gmail.com>
2024-05-08 11:59:18 +0700

    updated

---
+++ 8kyu/rpc.php
@@ -0,0 +1,16 @@
+<?php
+// Reversed string
+
+function rpc($p1, $p2)
+{
+    if ($p1 ==  $p2) {
+        return 'Draw!';
+    }
+    if (($p1 == 'rock' && $p2 == 'scissors') || ($p1 == 'scissors' && $p2 == 'paper') || ($p1 == 'paper' && $p2 == 'rock')) {
+        return "Player 1 won!";
+    } else {
+        return 'Player 2 won!';
+    }
+}
+
+echo rpc("rock", "scissors"); // Player 1 won!
